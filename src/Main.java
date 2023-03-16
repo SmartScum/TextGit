703ec86ac99cfd4147ba66fe5e9835753fedc18b
xhb <2772680691@qq.com>
2023-03-16 12:14:37 +0800

    修改分支

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
+    private static String name="夏海兵";
+    private static int age=18;
     public static void main(String[] args) {
 
-        System.out.println("5303的爹又来了！是周贤爷爷1");
+        System.out.println(name+"5303的爹又来了！是周贤爷爷!今年是18岁");
+        System.out.println("my name is "+name);
+        System.out.println("年龄："+age);
         System.out.println("我是分支");
     }
 
